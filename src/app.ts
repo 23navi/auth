6a479044b61d9120d4a68af8d3d08f7edd267d25
23navi <navisureka23@gmail.com>
2023-10-07 12:13:27 +0530

    logger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 require("dotenv").config();
 import express from "express";
 import config from "config";
+import connectToDb from "./utils/connectToDb";
+import log from "./utils/logger";
+
 const port = config.get<number>("port");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  log.info(`Server is running on port ${port}`);
+  connectToDb();
 });
