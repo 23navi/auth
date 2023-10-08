446182ebcf508037db83ae04ce101854ee7fd7ed
23navi <navisureka23@gmail.com>
2023-10-08 20:15:32 +0530

    validateResources middleware

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import config from "config";
 const router = express.Router();
+import { createUserSchema } from "../schemas/user.schema";
+import validateRequest from "../middlewares/validateResources";
+import { creatUserHandler } from "../controllers/user.controller";
+
+router.post("/", validateRequest(createUserSchema), creatUserHandler);
 
 export default router;
