cc2c5dba14d48483fdaf8ff3f2cd457d93b8f1f2
23navi <navisureka23@gmail.com>
2023-10-08 21:02:18 +0530

    mailer secret and transporter

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import config from "config";
 
 //We can go to the website: 'https://ethereal.email' and get the cred, or use the below to get cred. One time only
 // async function createTestCreds() {
@@ -6,5 +7,24 @@ import nodemailer from "nodemailer";
 //   console.log({ creds });
 // }
 // createTestCreds();
+
+type Tsmtp = {
+  username: string;
+  pass: string;
+  port: number;
+  host: string;
+  secure: boolean;
+};
+
+const smpt = config.get<Tsmtp>("smtp");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "alycia.reinger26@ethereal.email",
+    pass: "xgYAgGdqKvJSAFHS2y",
+  },
+});
 
 export const sendEmail = () => {};
