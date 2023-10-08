e60204746c0eee93725e99d2eda001b4331c15e8
23navi <navisureka23@gmail.com>
2023-10-08 20:37:46 +0530

    refactor nanoid import bug. Revert to @3.x.x

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { CreateUserInput } from "../schemas/user.schema";
+import { createUser } from "../services/user.service";
 
 export async function creatUserHandler(
   req: Request<{}, {}, CreateUserInput>,
@@ -10,6 +11,15 @@ export async function creatUserHandler(
 
   //   // We could have also done. // but as cast which is not good
   //   const body2 = req.body as CreateUserInput;
+  try {
+    const user = await createUser(body);
+    return res.status(201).send(user);
+  } catch (e: any) {
+    if (e.code === 11000) {
+      return res.status(409).send("User already exists");
+    }
+    return res.status(500).send(e);
+  }
 
   res.send(body);
 }
