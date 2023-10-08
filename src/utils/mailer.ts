814c831feee8a1d45a1784769554de63931c7271
23navi <navisureka23@gmail.com>
2023-10-08 20:58:39 +0530

    mailer util init

---
+++ src/utils/mailer.ts
@@ -0,0 +1,10 @@
+import nodemailer from "nodemailer";
+
+//We can go to the website: 'https://ethereal.email' and get the cred, or use the below to get cred. One time only
+// async function createTestCreds() {
+//   const creds = await nodemailer.createTestAccount();
+//   console.log({ creds });
+// }
+// createTestCreds();
+
+export const sendEmail = () => {};
