06ba9a27fd885e1a7e3514fe0ec9b65bac46fe92
23navi <navisureka23@gmail.com>
2023-10-30 17:20:05 +0530

    auth controller setup

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,32 @@
 // All the strategies will go here, like email/password, google login, facebook ....
 
 // We will only have email password strategy
+
+import { Request, Response } from "express";
+import { CreateSessionInput } from "../schemas/auth.schema";
+import { findUserByEmail } from "../services/user.service";
+
+export async function creatSessionHandler(
+  req: Request<{}, {}, CreateSessionInput>,
+  res: Response
+) {
+  const { email, password: candidatePassword } = req.body;
+  const user = await findUserByEmail(email);
+  if (!user) {
+    return res.json({ message: "Email/password not correct" });
+  }
+  if (user.verified) {
+    return res.json({ message: "Verify your account first" });
+  }
+  if (!(await user.validatePassword(candidatePassword))) {
+    return res.json({ message: "Email/password not correct" });
+  }
+
+  /// If we reach here, we will know that user provided correct creds, we need to provide them with token.
+
+  // 1) Sign access token
+
+  // 2) Sign refresh token
+
+  // 3) Send tokens back
+}
--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -5,15 +5,9 @@ export const createSessionSchema = object({
     password: string({
       required_error: "Password is required",
     }).min(6, "Password too short - should be 6 chars minimum"),
-    passwordConfirmation: string({
-      required_error: "Password confirmation is required",
-    }),
     email: string({
       required_error: "Email is required",
     }).email("Not a valid email"),
-  }).refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords do not match",
-    path: ["passwordConfirmation"],
   }),
 });
 export type CreateSessionInput = TypeOf<typeof createSessionSchema>["body"];
